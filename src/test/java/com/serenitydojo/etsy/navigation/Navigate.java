6682ef430a0ac3f71dc3e13661948b00ea09775e
John Ferguson Smart <john.smart@wakaleo.com>
2020-11-13 09:16:03 +0000

    Run the tests in headless mode for the Github actions

--- src/test/java/com/serenitydojo/etsy/navigation/Navigate.java
+++ src/test/java/com/serenitydojo/etsy/navigation/Navigate.java
@@ -5,12 +5,18 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.DoubleClick;
 import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.conditions.Check;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isCurrentlyVisible;
+import static net.serenitybdd.screenplay.questions.WebElementQuestion.the;
 
 public class Navigate {
     public static Performable toTheHomePage() {
 
         return Task.where("{0} opens the Etsy home page",
-                Open.url("https://www.etsy.com/").then(DoubleClick.on(CookieDialog.ACCEPT_BUTTON))
+                Open.url("https://www.etsy.com/"),
+                Check.whether(the(CookieDialog.ACCEPT_BUTTON), isCurrentlyVisible())
+                        .andIfSo(DoubleClick.on(CookieDialog.ACCEPT_BUTTON))
         );
     }
 
